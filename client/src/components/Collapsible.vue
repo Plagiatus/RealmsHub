3c77e1bb3094ceed4e9f20bef7c3ea56a32fa857
Lukas Scheuerle (Plagiatus) <lukas@spark-squared.com>
2021-10-01 12:07:35 +0300

    add discord and FAQ to frontpage

---
+++ client/src/components/Collapsible.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="collapsible">
+		<div class="collapsible-headline" :class="{'collapsible-open': open}" @click="toggle">{{headline}}</div>
+		<div class="collapsible-content" :class="{'collapsible-visible': open}">{{content}}</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: "Collapsible",
+	props: ["headline","content"],
+	data(){
+		return {
+			open: false
+		}
+	},
+	methods: {
+		toggle() {
+			this.open = !this.open;
+		}
+	}
+})
+</script>
+
+<style>
+.collapsible {
+	text-align: left;
+	background-color: var(--btn-dark-green);
+}
+
+.collapsible-headline {
+	padding: 1em;
+	cursor: pointer;
+	color: var(--background);
+}
+
+.collapsible:nth-of-type(2n){
+	background-color: var(--highlight);
+}
+
+.collapsible-content {
+	display: none;
+	overflow: hidden;
+	padding: 1em;
+	background-color: var(--background);
+}
+
+.collapsible-visible {
+	display: block;
+}
+
+.collapsible-headline::after {
+	content: '\02795';
+	float: right;
+	color: var(--background);
+	margin-left: 5px;
+}
+.collapsible-open::after {
+	content: "\2796";
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,25 +3,54 @@
 		<h1>Realms Hub</h1>
 		<span class="affiliation">Not affiliated to or endorsed by Mojang or Microsoft.</span>
     <img class="logo" alt="Vue logo" src="../assets/logo.png">
-		<div class="preview-note"><span><strong>Please note:</strong><br> This Page is currently under active development and barely in an <strong>alpha</strong> state. <br>Expect things to break or not be finished.</span></div>
+		<div class="preview-note"><span><strong>Please note:</strong><br> This Page is currently under active development and barely in an <strong>alpha</strong> state. <br>Expect things to break or not be finished. Especially on mobile devices.</span></div>
 		<div>
 			<div id="login">
 				<h3>Login</h3>
 				<a href="/login"><img class="login" src="../assets/ms-symbollockup_signin_light.svg" alt="Login with Microsoft"></a>
 				<span>Don't have a Microsoft Account?<br> Use the <a href="https://plagiatus.net/tools/realmadmin">old page</a>.</span>
 			</div>
-
+		</div>
+		<div id="info">
+			<h2>Info</h2>
+			<p>Realms Hub is an independant web service that allows you to manage your Realm from the web.</p>
+			<p>A Realm is a Minecraft Server, hosted by Mojang / Microsoft, the creators of the best-selling game ever.</p>
+			<p>This service is developed and maintained by <a href="https://plagiatus.net" target="_blank" rel="noopener noreferrer">Plagiatus</a>.</p>
+		</div>
+		<div id="faq">
+			<h2>FAQ</h2>
+			<collapsible :headline="'What can I do with this?'" :content="'Realms Hub lets you do basically anything you can also do with your realm ingame. Plus some additional features you won\'t find ingame!'"/>
+			<collapsible :headline="'Why do I need to login with Microsoft?'" :content="'All Mojang games have recently been merged to Microsoft.\nThus you need to login with Microsoft to be able to access your game information and manage your realm.'"/>
+			<collapsible :headline="'What kind of data to you save on your servers?'" :content="'We only store what is absolutely necessary for the service to work correctly, which is limited to the authentication tokens we get from the various needed Microsoft Services. As soon as you logout, all data on our servers will be deleted.'"/>
+			<collapsible :headline="'I\'ve found a bug. How can I report it?'" :content="'To submit a bug, please join our discord or send an email to contact[at]plagiatus[dot]net. Please include what you were trying to do and what happened instead. If you got an error popup, a screenshot of that would be appreciated.'"/>
+			<collapsible :headline="'How can I submit a feature request?'" :content="'To submit a feature request, please join our discord server or send an email to contact[at]plagiatus[dot]net. Please be aware that this is just a third party display and that we have no control over how the actual realm works!'"/>
+		</div>
+		<div id="discord">
+			<h2>Discord</h2>
+			<p>Join our <a href="https://discord.gg/upVpFQD" target="_blank" rel="noopener noreferrer">discord server</a> to get the latest updates on the website, submit bugs, request new features or just hang out with likeminded Minecraft Realms players.</p>
+			<a href="https://discord.gg/upVpFQD" target="_blank" rel="noopener noreferrer">
+				<img src="../assets/Discord-Logo+Wordmark-Color.svg" alt="Discord Logo">
+			</a>
 		</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { defineComponent } from '@vue/runtime-core';
+import Collapsible from '../components/Collapsible.vue';
 
-export default class Home extends Vue {}
+export default defineComponent({
+	components: {
+		Collapsible
+	}
+})
 </script>
 
 <style scoped>
+	.home {
+		width: 800px;
+	}
+
 	img.logo {
 		width: 200px;
 		height: 200px;
